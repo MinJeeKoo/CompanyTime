880094e0d7e73de132f11588120055821df306f6
MinJeeKoo <you@example.com>
2019-11-14 10:37:15 +0900

    min

--- DTO/CompanyReview_Service/src/dto/DepartmentDTO.java
+++ DTO/CompanyReview_Service/src/dto/DepartmentDTO.java
@@ -3,9 +3,9 @@ package dto;
 public class DepartmentDTO {
 	
 	private Integer CFD_NUM = null;
+	private Integer C_NUM = null;
 	private Integer CF_NUM = null;
 	private String CFD_NAME = null;
-	private Integer ANNULAL_INCOME = null;
 	
 	public Integer getCFD_NUM() {
 		return CFD_NUM;
@@ -14,6 +14,12 @@ public class DepartmentDTO {
 		this.CFD_NUM = CFD_NUM;
 	}
 	
+	public Integer getC_NUM() {
+		return C_NUM;
+	}
+	public void setC_NUM(Integer c_NUM) {
+		C_NUM = c_NUM;
+	}
 	public Integer getCF_NUM() {
 		return CF_NUM;
 	}
@@ -29,10 +35,5 @@ public class DepartmentDTO {
 		this.CFD_NAME = CFD_NAME;
 	}
 	
-	public Integer getANNULAL_INCOME() {
-		return ANNULAL_INCOME;
-	}
-	public void setANNULAL_INCOME(Integer ANNULAL_INCOME) {
-		this.ANNULAL_INCOME = ANNULAL_INCOME;
-	}
+	
 }
