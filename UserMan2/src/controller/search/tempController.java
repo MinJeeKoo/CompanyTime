646f2720328616fa185525dbbcd5dcedde83a312
kyounje <kyounje@naver.com>
2019-11-24 03:27:40 +0900

    최종 직전!

--- UserMan2/src/controller/search/tempController.java
+++ UserMan2/src/controller/search/tempController.java
@@ -1,12 +1,18 @@
 package controller.search;
-
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.Controller;
+import controller.DispatcherServlet;
+import model.service.UserManager_search_rs;
+import model.*;
 
 public class tempController implements Controller {
-
+	private final static Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
@@ -19,7 +25,15 @@ public class tempController implements Controller {
 		request.setAttribute("field", field);
 		request.setAttribute("dept", dept);
 		
-		return "/temp.jsp";
+		UserManager_search_rs manager = UserManager_search_rs.getInstance();
+		
+		List<ForSearchList> getSearchList = manager.get_SearchList(dept, category);
+		
+		logger.debug("getSearchList : {}", getSearchList);
+		
+		request.setAttribute("getSearchList", getSearchList);
+		
+		return "/search/temp.jsp";
 	}
 
 }
--- UserMan2/src/model/dao/InfoDAO.java
+++ UserMan2/src/model/dao/InfoDAO.java
@@ -1,9 +1,12 @@
 package model.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import model.*;
 
 public interface InfoDAO {
-	public Integer insertInfo(InfoDTO Info);	
+	public int insertInfo(InfoDTO Info) throws SQLException;	
 	public Integer updateInfo(InfoDTO Info);	
 	public Integer deleteInfo(Integer i_num);		
 	public InfoDTO getInfoByName(String iName);
@@ -11,4 +14,5 @@ public interface InfoDAO {
 	public InfoDTO getInfoByW_id(String w_id);
 	public Integer getI_numByP_id(String p_id);
 	public Integer getI_numByW_id(String w_id);
+	public List<ForSearchList> getSearchList(String cfd_name, String category) throws SQLException;
 }
